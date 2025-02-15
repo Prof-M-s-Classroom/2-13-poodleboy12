c599b1869037026e0b93c909c90d5483384ab3e7
poodleboy <beigehontal@gmail.com>
2025-02-15 14:26:22 -0800

    Assignment 2

--- main.cpp
+++ main.cpp
@@ -22,6 +22,9 @@ public:
         this->next = NULL;
     }
     void print() { value->print(); }
+    ~Node() {
+        delete value;
+    }
 };
 
 template <typename T> class LinkedList {
@@ -34,6 +37,7 @@ public:
         this->length = 1;
         this->head = new Node<T>(value);
     }
+    // If head is null there is problems
     void add(T *value) {
         Node<T> *newNode = new Node<T>(value);
         Node<T> *temp = head;
@@ -50,32 +54,106 @@ public:
         head = newNode;
         length++;
     }
-
+// If the head is null there will be a problem.
     void delfirst() {
         Node<T> *temp = head;
         head = head->next;
         delete temp;
         length--;
     }
-
+// If head is null most of these methods will have problems like this one.
     void dellast() {
         Node<T> *temp = head;
-        while (temp->next!=NULL)
-            temp = temp->next;
-        delete temp;
-        length--;
+        if (head == nullptr) {
+            return;
+        }
+        else if (head->next == nullptr) {
+            delete head;
+            head = nullptr;
+        }
+        else {
+            Node<T> *prev = nullptr;
+            while (temp->next != nullptr) {
+                prev = temp;
+                temp = temp->next;
+            }
+            delete temp;
+            prev->next = nullptr;
+            length--;
+        }
     }
 
     void deleteNode(int index) {
        //TODO:Write the function to delete at the given index. Reuse the pre-written functions for edge cases. Account for missing index.
+        Node<T> *temp = head;
+        int currIndex;
+        if (index == 0) {
+            head = temp->next;
+            delete temp;
+            length--;
+        }
+        else {
+            currIndex = 0;
+            while(temp != nullptr && currIndex != index - 1) {
+                temp = temp->next;
+                ++currIndex;
+            }
+            if (temp != nullptr) {
+                Node<T> *temp1 = temp->next;
+                temp->next = temp1->next;
+                delete temp1;
+                length--;
+            }
+        }
+    }
+ // You don't know when head is null. If head is null there is an issue.
+    Node<T>* get(int index) {
+        if(index < 0 || index>=length)
+            return nullptr;
+        Node<T>* temp = head;
+        for(int i = 0; i < index; i++)
+            temp=temp->next;
+
+        return temp;
     }
 
-   void insert(int index, T *value) {
-        //TODO:Write a function to insert a new node at a give index. Reuse the pre-written functions for edge cases. Account for missing index
+    // Your code will fail if head is null.
+    void insert(int index, T* value) {
+        if (index < 0 || index > length) {
+            cout << "Index is invalid" << endl;
+            return; // You need it here for an error case.
+        }
+        if (index==0){
+            addhead(value);
+        }
+        if(index==length) {
+            add(value);
+        }
+        else {
+            Node<T>* newNode = new Node<T>(value);
+            Node<T>* temp = get(index-1);
+            newNode->next = temp->next;
+            temp->next = newNode;
+            length++;
+        }
+        return; // Return doesn't matter because it will return even if it doesn't exist.
     }
 
-   void reverselist(){
+
+    void reverselist(){
         //TODO:Write a function to reverse the list using the logic from the slide.
+        Node<T>* prev;
+        Node<T>* curr;
+        Node<T>* next;
+        prev = nullptr;
+        curr = head;
+        while (curr!=nullptr) {
+            next = curr->nextNode;
+            curr->nextNode = prev;
+            prev = curr;
+            curr = next;
+        }
+        head = prev;
     }
 
     void print() {
